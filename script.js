08fe36e8e76c0fafe00b44b345d10afdfb8699ed
Sahil Patel <125381319+SahilPatel22@users.noreply.github.com>
2025-10-17 13:10:37 +0530

    New Concept added

--- script.js
+++ script.js
@@ -68,3 +68,131 @@ select.addEventListener("change", function(e){
     console.log(e.target.value);
 });
 
+
+// Keyboard event listener
+
+let h3 = document.querySelector("h3");
+window.addEventListener("keydown", function(e){
+    h3.textContent = e.key ;
+});
+
+// form event listener
+// let form = document.querySelector("form");
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();});
+
+// Forms and Form Validations
+// form validation-: form submit hone se pehle check karna ki form me sahi data dala hai ya nahi
+// required, type, pattern, min, max, minlength, maxlength
+
+let nm = document.querySelector("#name");
+let EM = document.querySelector("#email");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault(); // form submit hone se roko
+
+    // Name validation
+  if (nm.value.length <= 2) {
+    document.querySelector("#NAMEID").style.display = "inline";
+  }
+
+   // Email validation
+    // This regex is give by chatGPT
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(EM.value)) {
+    document.querySelector("#EmailID").style.display = "inline";
+  }
+
+  //password validation
+  // These regex is give by chatGPT
+  const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if(!strongPasswordRegex.test(password.value)){
+    document.querySelector("#PasswordID").style.display = "inline";
+  }
+});
+
+// Timers and Intervals
+// setTimeout, clearTimeout, setInterval, clearInterval 
+// setTimeout-: ye function ek baar specified time ke baad call hota hai
+// clearTimeout-: ye function setTimeout ko rokta hai
+// setInterval-: ye function specified time ke baad baar baar call hota hai
+// clearInterval-: ye function setInterval ko rokta hai 
+
+// setTimeout example
+// ye ek bar chalta he
+let timeoutId = setTimeout(function(){
+    console.log("Hello World");
+}, 3000);
+
+// clearTimeout example
+clearTimeout(timeoutId);
+console.log("Timeout cleared");
+
+// setInterval example
+// ye baar baar chalta he
+let count = 0;      
+let intervalId = setInterval(function(){
+    count++;
+    console.log("Count: " + count);
+}, 1000);
+
+// clearInterval example
+setTimeout(function(){
+    clearInterval(intervalId);
+    console.log("Interval cleared");
+}, 5000);
+  
+// seesion storage and local storage and cookies
+// local storage-: ye data ko browser me store karta hai aur data tab tak rehta hai jab tak user manually data ko delete nahi karta
+// local storage and session storage me data key-value pair me store hota hai
+// local storage and session storage me sirf string hi store hota hai
+// local storage and session storage me data ko set, get, remove, clear kar sakte hai
+// session storage-: ye data ko browser me store karta hai aur data tab tak rehta hai jab tak browser tab close nahi hota
+
+//local storage example
+/// Store kaise kare
+localStorage.setItem("name", "John Doe");
+
+// data fecth kaise kare
+let val = localStorage.getItem("name");
+console.log(val);
+
+// remove kaise kare
+localStorage.removeItem("name");
+
+// clear kaise kare
+localStorage.clear();
+
+
+// session storage example
+// Store kaise kare
+sessionStorage.setItem("sessionName", "Jane Doe");
+
+// data fecth kaise kare
+let sessionVal = sessionStorage.getItem("sessionName");
+console.log(sessionVal);
+
+// remove kaise kare
+sessionStorage.removeItem("sessionName");
+
+// clear kaise kare
+sessionStorage.clear();
+
+
+// cookies-: ye data ko browser me store karta hai aur data tab tak rehta hai jab tak cookie expire nahi hoti
+// cookies me data key-value pair me store hota hai
+// cookies me sirf string hi store hota hai
+// cookies me data ko set, get, remove kar sakte hai
+// cookies me data ko expire time ke sath set kar sakte hai
+// cookies me jo page reload hone par data server ko bheja jata hai
+
+// cookies example
+
+// set cookie
+document.cookie = "username=John Doe; expires=Fri, 31 Dec 2024 23:59:59 GMT; ";
+
+// get cookie
+let cookies = document.cookie;
+console.log(cookies); 
+
